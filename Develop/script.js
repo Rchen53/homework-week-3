6076a0f6b419ad468ad954c8b6aa96819c69a9f9
rchen53 <rchen53@binghamton.edu>
2020-02-28 00:06:24 -0500

    final with working generate btn, working user preference, and password in text

--- Develop/script.js
+++ Develop/script.js
@@ -3,125 +3,123 @@ var randomUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var randomLower = 'abcdefghijklmnopqrstuvwxyz';
 var randomNumber = '0123456789';
 var randomSymbol = '!@#$%^&*()_-=+{}:;?/><,.';
-
-
-
+ 
+ 
 //get user number input for number between 8 and 128
 function getLength() {
   //prompt for the input
   var userLength = prompt('Please enter a number between 8 and 128!');
-
+ 
   //parse the string and turn it to an integer
   var parsedNum = parseInt(userLength);
-
+ 
   //if NaN or Num lower than 8 or Num greater than 128, return function
   if (!parsedNum || parsedNum < 8 || parsedNum > 128){
     return getLength();
-
+ 
     //else return the parsednum and store it into variable
   } else {
     return parsedNum;
   }
 }
-//log to confirm that the code is working as it intended to
 
-charLength = getLength();
-
-//create a function that returns a lowercase letter
+//store number in a variable
+var charLength = getLength();
+ 
+//Store boolean result of lower in a variable
 var confirmLower = confirm('Do you want lowercase in your passwords? Press "OK" for "YES" and "Cancel" for "NO!"');
-  if (confirmLower === true) {
-    randomLower;
-  } else {
-    "";
-  }
-console.log(confirmLower);
 
-//create a function that returns a uppercase letter
+
+ 
+//Store boolean result of upper in a variable
 var confirmUpper = confirm('Do you want uppercases in your passwords? Press "OK" for "YES" and "Cancel" for "NO!"');
-  if (confirmUpper === true) {
-    randomUpper;
-  } else {
-    "";
-  }
-console.log(confirmLower);
-
-//create a function that returns a number
+  
+ 
+//Store boolean result of number in a variable
 var confirmNumber = confirm('Do you want numbers in your passwords? Press "OK" for "YES" and "Cancel" for "NO!"');
-  if (confirmNumber === true) {
-    randomNumber;
-  } else {
-    "";
-  }
-console.log(confirmNumber);
 
-//create a function that returns a symbol
+
+
+ 
+//store boolean result of symbol in a variable
 var confirmSymbol = confirm('Do you want symbols in your passwords? Press "OK" for "YES" and "Cancel" for "NO!"');
-  if (confirmSymbol === true) {
-    randomSymbol;
+
+
+
+//create a function that generate the character types to be include in generating a random password base on user preference
+ 
+function generateCharactersType() {
+  var charactersType = "";
+  if (confirmLower === true && confirmNumber === true && confirmSymbol === true && confirmUpper === true) {
+    return charactersType = randomLower + randomNumber + randomSymbol + randomUpper;
+  } else if (confirmLower === true && confirmNumber === true && confirmSymbol === true) {
+    return charactersType = randomLower + randomNumber + randomSymbol;
+  } else if (confirmLower === true && confirmNumber === true) {
+    return charactersType = randomLower + randomNumber;
+  } else if (confirmLower === true) {
+    return charactersType = randomLower;
+  } else if (confirmLower === true && confirmSymbol === true && confirmUpper === true) {
+    return charactersType = randomLower + randomSymbol + randomUpper;
+  } else if (confirmLower === true && confirmSymbol === true) {
+    return charactersType = randomLower + randomSymbol;
+  } else if (confirmLower === true && confirmUpper === true) {
+    return charactersType = randomLower + randomUpper;
+  } else if (confirmNumber === true && confirmSymbol === true && confirmUpper === true) {
+    return charactersType = randomNumber + randomSymbol + randomUpper;
+  } else if (confirmNumber === true && confirmSymbol === true) {
+    return charactersType = randomNumber + randomSymbol;
+  } else if (confirmNumber === true) {
+    return charactersType = randomNumber;
+  } else if (confirmNumber === true && confirmUpper === true) {
+    return charactersType = randomNumber + randomUpper;
+  } else if (confirmUpper === true) {
+    return charactersType = randomUpper;
+  } else if (confirmUpper === true && confirmSymbol === true) {
+    return charactersType = randomUpper + randomSymbol;
+  } else if (confirmSymbol === true){
+    return charactersType = randomSymbol;
   } else {
-    "";
+    return alert('You must choose at least on character type') + document.location.reload();
   }
-console.log(confirmSymbol);
+}
 
-//create a function that returns password selected characters depending on specific user needs base on true or false statements
-
-
-
-
-
-charactersType = randomUpper + randomSymbol + randomNumber + randomLower;
-
-console.log(password(charLength, charactersType));
+//Store user preference into a variable
+var charactersType = generateCharactersType ();
 
 
 
-
-// Write password to the #password input
-function password(charLength, characters) {
+//Generate the password base on user input
+function generatePassword(charLength, characters) {
   var pwd = "";
   var passwordText = document.querySelector("#password");
-
-
-
-  for (var i = 0; i<charLength; i++) {
+ 
+  for (var i = 0; i < charLength; i++) {
     pwd += characters.charAt(Math.floor(Math.random() * characters.length));
   }
-
+  console.log(pwd);
   return pwd;
-  passwordText.value = password(charLength.value, charactersType);
-
 }
-
-
-// Add an event listener to generate button that generate the printed password onto the passwordText
-
-generateBtn.addEventListener("click", writePassword); {
-
-  var characters = char;
-  (confirmLower.checked) ? characters += randomLower : '';
-  (confirmSymbol.checked) ? characters += randomSymbol : '';
-  (confirmUpper.checked) ? characters += randomUpper : '';
-  (confirmNumber.checked) ? characters += randomNumber : '';
-
-
-  passwordText.value = password(charLength.value, charactersType);
-
-};
-
-generateBtn;
-
-
-
-/*
+ 
+ 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(charLength, charactersType);
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+ 
+  passwordText.textContent = password;
+ 
 }
-
+ var generateBtn = document.getElementById('generate');
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-*/
+
+generateBtn.addEventListener("click", writePassword());
+
+
+//reload the page everytime generate password button is clicked
+var generateBtnClick = document.getElementById('generate');
+
+generateBtnClick.addEventListener('click', function(){
+  if (true){
+    return document.location.reload();
+  }
+})
